a222b72cefed28ccb80e2074d68023d87de71daf
gustavruas <gustavruas@hotmail.com>
2025-10-16 17:31:23 -0300

    Array Em Todas as funções

---
+++ testlib.h
@@ -0,0 +1,462 @@
+#include <stdio.h>
+#include <math.h> 
+#include <stdlib.h> // Malloc e e free; vulgo, funções para controle de memória
+
+#define PI 3.14159265
+
+int *array; // Aponta pro endereço da memória, famoso ponteiro
+int tamanho; // Define o tamanho que será mudado, tornando o array dinamico
+
+int ab32(){
+    printf("Tamanho do array:");
+    scanf("%d", &tamanho); // pede o tamanho do array para o usuário
+    if(tamanho < 0 || tamanho == 0){
+        printf("Selecione um tamanho maior que 0");
+        return 1;
+    } else{
+    array = (int *) malloc(tamanho * sizeof(int)); // Alocação dinamica de memoria, aprofundar em malloc
+
+    if(array == NULL){
+        printf("Erro ao alocar memoria. \n");
+        return 1; // Botar return 1 quando for indicar algum erro, na estrutura if + printf + return 1
+    }
+    printf("Digite %d elementos:\n ",tamanho);
+    return 0;
+}}
+
+void ler_array(){
+    for(int i = 0; i < tamanho; i++){
+        printf("Elemento %d: ", i + 1); // o %d é qualquer decimal que será representado depois da vírgula
+        scanf("%d", &array[i]); // Não entendi o "&"
+    }
+}
+
+void soma(){
+    double res = 0;
+    if(ab32() != 0) {   
+        return;
+    }
+    ler_array();
+    for(int i=0; i<tamanho; i++){
+        res += array[i]; // Soma os elementos à soma, um por um
+    } 
+
+    printf("Soma = %lf\n", res);
+    free(array); // Libera memória
+}
+
+void subtrair(){
+    double res = 0;
+    if(ab32() != 0) {      
+       return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+       res -= array[i]; // subtrai os elementos à soma, um por um
+    }
+
+    printf("Subtracao = %lf", res);
+    free(array);
+}
+
+void multiplicar(){
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= array[i]; 
+    }
+    printf("Multiplicacao = %lf", res);
+    free(array);
+}
+
+void dividir(){
+    if(ab32() != 0) {       
+        return;
+    }
+    double res = (double)array[0]; // Definindo como primeiro item pra ficar como input/input ao invés de 1/input/input... Alías também convertendo o seu tipo int -> double
+    ler_array(); 
+    for(int i = 0; i < tamanho; i++){
+        if(array[i] == 0){
+            printf("Não divida por zero.");
+        } else{
+        res /= array[i];
+        }
+    }
+    printf("Divisao = %lf", res);
+    free(array);
+}
+
+void porcentagem(){ 
+    double a;
+    printf("Digite o numero base e as porcentagens no proximo prompt:");
+    scanf("%lf", &a);
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= (array[i]/100.0);
+    }
+    double resFinal;
+    resFinal = a*res;
+    printf("Resultado:%lf", resFinal);
+    free(array);
+}
+void potencia(){
+    double a;
+    printf("Digite o numero base e os expoentes no proximo prompt:");
+    scanf("%lf", &a);
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= array[i];
+    }
+    double resFinal = pow(a,res);
+    printf("Resultado:%lf", resFinal);
+    free(array);
+    // Tentar fazer sem a biblioteca
+}
+void raiz2(){
+    if(ab32() != 0) {      
+        return;
+    }
+    ler_array();
+    double res;
+    double a;
+    printf("Base:");
+    scanf("%lf", &a);
+    res = pow(a, 0.5);
+    if(a < 0){
+        printf("Nao trataremos numeros complexos ou strings");
+    } else{
+    printf("Resultado:%lf", res);
+    }
+}
+void raiz3(){
+    if(ab32() != 0) {      
+        return;
+    }
+    ler_array();
+    double res;
+    double a;
+    double b = 1.0/3.0;
+    printf("Base:");
+    scanf("%lf", &a);
+    if(a < 0){
+        printf("Nao trataremos numeros complexos ou strings");
+    } else {
+    res = pow(a, b);
+    printf("Resultado:%lf", res);
+    }
+}
+void potenciareversa(){
+   double a;
+    printf("Digite o numero base e os expoentes no proximo prompt:");
+    scanf("%lf", &a);
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= array[i];
+    }
+    double resFinal = pow(a,-res);
+    printf("Resultado:%lf", resFinal);
+    free(array);
+}
+void fatorial(){
+    double res;
+    int a;
+    printf("Selecione um numero");
+    scanf("%d", &a);
+    res = tgamma(a+1);
+    if (a < 0){
+        printf("Fatorial nao eh feito pra numeros negativos");
+    }
+    else{
+    printf("Fatorial de %d é: %lf\n", a, res);
+    }
+}
+
+void s3n(){
+    double angulo;
+    double res;
+    double c;
+    printf("Selecione o angulo:");
+    scanf("%lf", &angulo);
+    res = angulo*PI/180.0; // Definindo radianos
+    c = sin(res);
+    printf("%lf",c);
+}
+void c0s(){
+    double angulo;
+    double res;
+    double c;
+    printf("Selecione o angulo:");
+    scanf("%lf", &angulo);
+    res = angulo*PI/180.0; // Definindo radianos
+    c = cos(res);
+    printf("%lf",c);
+}
+void t4n(){
+    double angulo;
+    double res;
+    double c;
+    printf("Selecione o angulo:");
+    scanf("%lf", &angulo);
+    res = angulo*PI/180.0; // Definindo radianos
+    c = tan(res);
+    printf("%lf",c);
+}
+void secante(){
+    double angulo;
+    double res;
+    double c;
+    printf("Selecione o angulo:");
+    scanf("%lf", &angulo);
+    res = angulo*PI/180.0; // Definindo radianos
+    c = 1/sin(res);
+    printf("%lf",c);
+}
+void cossecante(){
+    double angulo;
+    double res;
+    double c;
+    printf("Selecione o angulo:");
+    scanf("%lf", &angulo);
+    res = angulo*PI/180.0; // Definindo radianos
+    c = 1/cos(res);
+    printf("%lf",c);
+}
+void contangente(){
+    double angulo;
+    double res;
+    double c;
+    printf("Selecione o angulo:");
+    scanf("%lf", &angulo);
+    res = angulo*PI/180.0; // Definindo radianos
+    c = 1/tan(res);
+    printf("%lf",c);
+}
+void logaritmo(){
+    double base;
+    double numero;
+    double res;
+    printf("Selecione a base:");
+    scanf("%lf", &base);
+    printf("Selecione o numero:");
+    scanf("%lf", &numero);
+    if(base > 1 && numero > 1){
+        res = log(numero) / log(base);
+    }
+    else{
+        printf("Input invalido(string ou num negativo)");
+        return;
+    }
+}
+void log_natural(){
+    double numero;
+    double res;
+    printf("Selecione o numero:");
+    scanf("%lf", &numero);
+    if(numero > 1){
+    res = log(numero);
+    printf("%lf", res);
+    }
+    else{
+        printf("Selecione um input valido(Erro por string ou por numero negativo)");
+        return;
+    }
+}
+void modulo(){
+    int a;
+    printf("Selecione um numero:");
+    scanf("%d", &a);
+    if (a > 0 || a < 0 || a==0){
+      int res = abs(a);
+      printf("O valor absoluto de %d eh %d", a, res);
+    } else {
+        printf("Numero invalido");
+    }
+}
+void dezElevadoaX(){
+    printf("10 elevado a qual expoente:\n");
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= array[i];
+    }
+    double resFinal = pow(10,res);
+    printf("Resultado:%lf", resFinal);
+    free(array);
+}
+void vezesEuler(){
+double a;
+    printf("Digite os multiplicadores no proximo prompt:\n");
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= array[i];
+    }
+    double resFinal = exp(1)*res;
+    printf("Resultado:%lf", resFinal);
+    free(array);
+}
+void EulerElevado(){
+double a;
+    printf("Digite os expoentes no proximo prompt:");
+    double res = 1;
+    if(ab32() != 0) {       
+        return;
+    }
+    ler_array();
+    for(int i = 0; i < tamanho; i++){
+        res *= array[i];
+    }
+    double resFinal = pow(exp(1),res);
+    printf("Resultado:%lf", resFinal);
+    free(array);
+}
+
+void media(){
+    double res;
+    if(ab32() != 0){
+        return;
+    }
+    for(int i = 0; i < tamanho; i++){
+        printf("Elemento %d ", i + 1);
+        scanf("%d", &array[i]);
+           res += array[i]; 
+     }    
+     double d = res/tamanho;
+     printf("%lf", d);
+     free(array);
+}
+
+void maximo(){
+    if(ab32() != 0){
+        return;
+    }
+    int max_val = array[0];
+    for(int i = 1; i < tamanho; i++){
+        printf("Elemento %d: ", i + 1);
+        scanf("%d", &array[i]);
+        if(array[i] > max_val){
+            max_val = array[i];
+        }
+    }
+    printf("O valor maximo eh: %d\n", max_val);
+    free(array);
+}
+
+void menu(){
+    int escolha; 
+        printf("\n+-------------------------------------------------------------+\n");
+        printf("|  1. Soma           |  2. Subtrair      |  3. Multiplicar    |\n");
+        printf("|  4. Dividir        |  5. Porcentagem   |  6. Potencia       |\n");
+        printf("|  7. Raiz quadrada  |  8. Raiz cubica   |  9. Pot. reversa   |\n");
+        printf("| 10. Fatorial       | 11. Seno          | 12. Cosseno        |\n");
+        printf("| 13. Tangente       | 14. Secante       | 15. Cossecante     |\n");
+        printf("| 16. Cotangente     | 17. Log base N    | 18. Log natural    |\n");
+        printf("| 19. Modulo         | 20. 10^X          | 21. X * Euler      |\n");
+        printf("| 22. Euler^X        | 23. Media         | 24. Max            |\n");
+        printf("|                    0. Sair                                  |\n");
+        printf("+-------------------------------------------------------------+\n");
+        
+        printf("Escolha uma opcao:");        
+
+        scanf("%d", &escolha);
+
+    switch (escolha)
+    {
+    case 1:
+        soma();
+        break;
+    case 2:
+        subtrair();
+        break;
+    case 3:
+        multiplicar();
+        break;
+    case 4:
+        dividir();
+        break;
+    case 5:
+        porcentagem();
+        break;
+    case 6:
+        potencia();
+        break;
+    case 7:
+        raiz2();
+        break;
+    case 8:
+        raiz3();
+        break;
+    case 9:
+        potenciareversa();
+        break;
+    case 10:
+        fatorial();
+        break;
+    case 11:
+        s3n();
+        break;
+    case 12:
+        c0s();
+        break;
+    case 13:
+        t4n();
+        break;
+    case 14:
+        secante();
+        break;
+    case 15:
+        cossecante();
+        break;
+    case 16:
+        contangente();
+        break;
+    case 17:
+        logaritmo();
+        break;
+    case 18:
+        log_natural();
+        break;
+    case 19:
+        modulo();
+        break;
+    case 20:
+        dezElevadoaX();
+        break;
+    case 21:
+        vezesEuler();
+        break;
+    case 22:
+        EulerElevado();
+        break;
+    case 23:
+        media();
+        break;
+    case 24:
+        maximo();
+        break;
+    default:
+        printf("Selecione alguma funcao do menu! Fechando programa.\n");
+        break;
+    }
+}
